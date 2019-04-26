58662116e384fc57b1826c12a9078981b729459c
Ilyaki <ilyaki.ilyaki@gmail.com>
2019-04-26 17:00:04 +0100

    optimized ServerInterface

--- GetRawInputDataHook/ServerInterface.cs
+++ GetRawInputDataHook/ServerInterface.cs
@@ -14,7 +14,7 @@ namespace GetRawInputDataHook
 		private int cursorX;
 		private int cursorY;
 		
-		private byte[] VKeysPressed = new byte[256];//Mouse VKeys are 0x01 to 0x06
+		private bool[] VKeysPressed = new bool[256];//Mouse VKeys are 0x01 to 0x06
 
 		public void SetToReleaseHook()
 		{
@@ -40,17 +40,12 @@ namespace GetRawInputDataHook
 
 		public void SetVKey(int VKey, bool isPressed)
 		{
-			VKeysPressed[VKey] = (byte)(isPressed ? 0b10000000 : 0);
+			VKeysPressed[VKey] = isPressed;
 		}
 
 		public bool GetIsVKeyPressed(int VKey)
 		{
-			return VKeysPressed[VKey] != 0;
-		}
-
-		public byte[] GetVKeyArray()
-		{
-			return VKeysPressed;
+			return VKeysPressed[VKey];
 		}
 
 		public void SetGame_hWnd(IntPtr hWnd)
@@ -66,7 +61,7 @@ namespace GetRawInputDataHook
 		public void IsInstalled(int clientPID)
 		{
 			Console.WriteLine("Injected hook(s) into process {0}.\r\n", clientPID);
-			VKeysPressed = new byte[256];
+			VKeysPressed = new bool[256];
 		}
 
 		public void ReportMessage(string message)
