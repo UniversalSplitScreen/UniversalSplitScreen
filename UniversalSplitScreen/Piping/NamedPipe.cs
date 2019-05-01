3f1798aa5a49ea716817a03662a1c63d51e19edb
Ilyaki <ilyaki.ilyaki@gmail.com>
2019-05-01 16:25:49 +0100

    Fixed named pipe blocking thread

--- UniversalSplitScreen/Piping/NamedPipe.cs
+++ UniversalSplitScreen/Piping/NamedPipe.cs
@@ -1,21 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO.Pipes;
+using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace UniversalSplitScreen.Piping
 {
 	public class NamedPipe
 	{
 		public readonly string pipeName;
-		
+
+		Thread serverThread;
 		NamedPipeServerStream pipeServer;
+		bool clientConnected = false;
 
 		public NamedPipe()
 		{
 			pipeName = GenerateName();
-			
-			Start();
+
+			serverThread = new Thread(Start);
+			serverThread.Start();
 		}
 
 		public void Start()
@@ -23,19 +30,24 @@ namespace UniversalSplitScreen.Piping
 			Console.WriteLine($"Starting pipe {pipeName}");
 			pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous);
 			Console.WriteLine($"Created pipe {pipeName}");
+
 			pipeServer.WaitForConnection();
+			clientConnected = true;
 			Console.WriteLine($"Client connected to pipe {pipeName}");
 		}
 
 		public void AddMessage(byte message, int param1, int param2)
 		{
-			byte[] bytes = {
-					message,
-					(byte)(param1 >> 24), (byte)(param1 >> 16), (byte)(param1 >> 8), (byte)param1,
-					(byte)(param2 >> 24), (byte)(param2 >> 16), (byte)(param2 >> 8), (byte)param2
-				};
+			if (clientConnected)
+			{
+				byte[] bytes = {
+						message,
+						(byte)(param1 >> 24), (byte)(param1 >> 16), (byte)(param1 >> 8), (byte)param1,
+						(byte)(param2 >> 24), (byte)(param2 >> 16), (byte)(param2 >> 8), (byte)param2
+					};
 
-			pipeServer.Write(bytes, 0, 9);
+				pipeServer.Write(bytes, 0, 9);
+			}
 		}
 
 		//https://github.com/EasyHook/EasyHook/blob/master/EasyHook/RemoteHook.cs
