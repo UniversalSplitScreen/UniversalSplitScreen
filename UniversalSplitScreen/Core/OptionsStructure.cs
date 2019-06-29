103c2d2d449c528694b62e46a81225e95ab48d44
Ilyaki <ilyaki.ilyaki@gmail.com>
2019-06-29 09:06:08 +0100

    Updated config and tooltips for absolute position flag

--- UniversalSplitScreen/Core/OptionsStructure.cs
+++ UniversalSplitScreen/Core/OptionsStructure.cs
@@ -29,7 +29,7 @@ namespace UniversalSplitScreen.Core
 		public bool Hook_GetKeyState				{ get; set; } = false;
 		public bool Hook_XInput						{ get; set; } = false;
 		public bool Hook_UseLegacyInput				{ get; set; } = false;
-		public bool UpdateAbsoluteFlagInMouseMessage { get; set; } = false; //Only does something if legacy input is on
+		public bool UpdateAbsoluteFlagInMouseMessage { get; set; } = true; //Only does something if legacy input is on
 		public bool Hook_MouseVisibility			{ get; set; } = false;
 
 		public ushort EndVKey						{ get; set; } = 0x23;
