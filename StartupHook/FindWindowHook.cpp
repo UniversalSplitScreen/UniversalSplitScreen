7e16fa0f627cc517e5311b6d698757467078d051
Ilyaki <ilyaki.ilyaki@gmail.com>
2019-08-08 17:50:01 +0200

    Removed unused RegisterRawInputDevices hook

--- StartupHook/FindWindowHook.cpp
+++ StartupHook/FindWindowHook.cpp
@@ -19,42 +19,6 @@ HANDLE WINAPI OpenProcess_Hook(DWORD dwDesiredAccess, BOOL bInheritHandle, DWORD
 	return NULL;
 }
 
-BOOL WINAPI RegisterRawInputDevices_Hook(
-	PCRAWINPUTDEVICE pRawInputDevices,
-	UINT             uiNumDevices,
-	UINT             cbSize
-)
-{
-	/*for (UINT i = 0; i < uiNumDevices; i++)
-	{
-		auto r = pRawInputDevices[i];
-		MessageBox(NULL, (std::to_string(r.dwFlags) + ", " + std::to_string((int)r.hwndTarget) + ", " + std::to_string(r.usUsage) + ", " + std::to_string(r.usUsagePage) + ", ").c_str(), "reg0", MB_OK);
-	}*/
-
-	auto p = static_cast<tagRAWINPUTDEVICE*>(malloc(uiNumDevices * cbSize));
-	//memcpy(p, pRawInputDevices, uiNumDevices * sizeof(tagRAWINPUTDEVICE));
-
-	auto rp = static_cast<PCRAWINPUTDEVICE>(p);
-	
-	for (UINT i = 0; i < uiNumDevices; i++)
-	{
-		auto cp = pRawInputDevices[i];
-
-		//cp.dwFlags |= RIDEV_INPUTSINK;
-		//cp.dwFlags &= ~(RIDEV_EXINPUTSINK);
-		cp.dwFlags = RIDEV_INPUTSINK;
-
-		memcpy(static_cast<void*>(p + i * sizeof(tagRAWINPUTDEVICE)), &cp, cbSize);
-	}
-
-	/*for (UINT i = 0; i < uiNumDevices; i++)
-	{
-		auto r = p[i];
-		MessageBox(NULL, (std::to_string(r.dwFlags) + ", " + std::to_string((int)r.hwndTarget) + ", " + std::to_string(r.usUsage) + ", " + std::to_string(r.usUsagePage) + ", ").c_str(), "reg0", MB_OK);
-	}*/
-	return RegisterRawInputDevices(p, uiNumDevices, cbSize);
-}
-
 void installFindWindowHooks()
 {
 	installHook("user32.dll", "FindWindowA", FindWindow_Hook);
@@ -63,6 +27,4 @@ void installFindWindowHooks()
 	installHook("user32.dll", "FindWindowExW", FindWindowEx_Hook);
 
 	installHook("kernel32.dll", "OpenProcess", OpenProcess_Hook);
-
-	installHook("User32.dll", "RegisterRawInputDevices", RegisterRawInputDevices_Hook);
 }
